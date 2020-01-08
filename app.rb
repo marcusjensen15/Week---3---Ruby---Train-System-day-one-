2a2634be22cf57994720a9824ce1537c16f69ae3
Student <example@example.com>
2020-01-08 14:14:07 -0800

    create and read functionality now working for both trains and cities

--- app.rb
+++ app.rb
@@ -20,19 +20,69 @@ get ('/home') do
   erb(:home)
 end
 
+#This shows the operator page
 get ('/home/operator') do
+  @trains = Train.all
+  @cities = City.all
   erb(:operator)
 end
+
+#this adds all the trains and cities for the operator view
+
+post ('/home/operator') do
+  if params[:train_name]
+      name = params[:train_name]
+      train = Train.new({:name => name, :train_id => nil})
+      train.save()
+      @trains = Train.all()
+      @cities = City.all()
+    erb(:operator)
+  elsif params[:city_name]
+    name = params[:city_name]
+    city = City.new({:name => name, :city_id => nil})
+    city.save()
+    @cities = City.all()
+    @trains = Train.all()
+
+  erb(:operator)
+  end
+end
+
+#this shows the page allowing the operator to add a new train
+
 
 get('/train/new') do
   erb(:new_train)
 end
 
-get('/trains/new') do
-  erb(:new_train)
+#this shows the page allowing the operator to add a new city
+get('/city/new') do
+  erb(:new_city)
+end
+
+#this shows the rider view
+get ('/home/rider') do
+  erb(:rider)
+end
+
+#this shows the page on the operator view for a specific train id
+
+get ('/home/operator/trains/:train_id')do
+  @train = Train.find(params[:train_id].to_i())
+
+  erb(:train)
+
 end
 
 
-get ('/home/rider') do
-  erb(:rider)
+
+#this shows the page on the operator view for a specific city id
+
+get ('/home/operator/cities/:city_id')do
+
+  @city = City.find(params[:city_id].to_i())
+
+  erb(:city)
+
+
 end
